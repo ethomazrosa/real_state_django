5c5e8e1a7686869875c0d240b6240648df8ed6b7
E. Thomaz Rosa <elcio.thomaz@gmail.com>
2023-03-13 15:07:17 -0300

    Add property added.

--- listings/admin.py
+++ listings/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 
-from .forms import ListingsForm
+# from .forms import ListingsForm
 from .models import Listing
 
 
-class ListingAdmin(admin.ModelAdmin):
-    form = ListingsForm
+# class ListingAdmin(admin.ModelAdmin):
+#     form = ListingsForm
 
 # Register your models here.
-admin.site.register(Listing, ListingAdmin)
+admin.site.register(Listing)
--- listings/models.py
+++ listings/models.py
@@ -1,11 +1,15 @@
+from django.contrib.auth import get_user_model
 from django.contrib.gis.db import models
 from django.contrib.gis.geos import Point
 from django.utils import timezone
+
+User = get_user_model()
 
 # Create your models here.
 
 
 class Listing(models.Model):
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
     title = models.CharField(max_length=150)
     description = models.TextField(null=True, blank=True)
     choices_area = (
@@ -43,7 +47,8 @@ class Listing(models.Model):
     cctv = models.BooleanField(default=False)
     parking = models.BooleanField(default=False)
     date_posted = models.DateTimeField(default=timezone.now)
-    location = models.PointField(blank=True, null=True, srid=4326)
+    latitude = models.FloatField(blank=True, null=True)
+    longitude = models.FloatField(blank=True, null=True)
     picture1 = models.ImageField(blank=True, null=True, upload_to='pictures/%Y/%m/%d/')
     picture2 = models.ImageField(blank=True, null=True, upload_to='pictures/%Y/%m/%d/')
     picture3 = models.ImageField(blank=True, null=True, upload_to='pictures/%Y/%m/%d/')
